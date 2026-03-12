c78148bd0efd5cc2afdad0b00c17fac5f1cfce37
Siddhi Amale <161148479+siddhiamale@users.noreply.github.com>
2026-03-12 10:18:53 +0530

    Update mpi_hello.c

--- OS-II/Assi 4/mpi_hello.c
+++ OS-II/Assi 4/mpi_hello.c
@@ -1,31 +1,17 @@
 #include <mpi.h>
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
+    int rank, size;
 
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Init(&argc, &argv);
 
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
+    printf("Hello World from process %d out of %d processes\n", rank, size);
 
-    MPI_Get_processor_name(processor_name, &name_len);
-
-    // Print hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
-
-    // Finalize the MPI environment
     MPI_Finalize();
-
     return 0;
 }
