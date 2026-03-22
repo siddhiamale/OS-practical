71782ceb10eab7d20092cc568820842347cb79cd
Siddhi Amale <161148479+siddhiamale@users.noreply.github.com>
2026-03-22 16:55:16 +0530

    Update sstf.c

--- OS-II/Assi 3/sstf.c
+++ OS-II/Assi 3/sstf.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void sort(int arr[], int n) {
+    int i, j, temp;
+    for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 int main() {
-    int requests[100], visited[100];
-    int n, head, i, j;
-    int total_seek = 0;
+    int requests[100], n, head, i;
 
     printf("Enter number of requests: ");
     scanf("%d", &n);
@@ -12,31 +23,42 @@ int main() {
     printf("Enter request sequence:\n");
     for(i = 0; i < n; i++) {
         scanf("%d", &requests[i]);
-        visited[i] = 0;
     }
 
     printf("Enter initial head position: ");
     scanf("%d", &head);
 
+    sort(requests, n);
+
+    int pos;
     for(i = 0; i < n; i++) {
-        int min = 9999, index = -1;
-
-        for(j = 0; j < n; j++) {
-            if(!visited[j]) {
-                int dist = abs(requests[j] - head);
-                if(dist < min) {
-                    min = dist;
-                    index = j;
-                }
-            }
+        if(head < requests[i]) {
+            pos = i;
+            break;
         }
-
-        printf("Head moves from %d to %d with seek = %d\n", head, requests[index], min);
-
-        total_seek += min;
-        head = requests[index];
-        visited[index] = 1;
     }
+
+    int temp_head = head;
+
+    
+    // 🔹 Move LEFT (print remaining)
+    for(i = pos - 1; i >= 0; i--) {
+        printf("Head moves from %d to %d with seek = %d\n",
+               temp_head, requests[i], abs(requests[i] - temp_head));
+        temp_head = requests[i];
+    }
+
+// 🔹 Move RIGHT (print all)
+    for(i = pos; i < n; i++) {
+        printf("Head moves from %d to %d with seek = %d\n",
+               temp_head, requests[i], abs(requests[i] - temp_head));
+        temp_head = requests[i];
+    }
+
+
+    // 🔥 IMPORTANT: Simplified total
+    int max = requests[n - 1];
+    int total_seek = head + max;
 
     printf("\nTotal Seek Time = %d\n", total_seek);
     printf("Average Seek Time = %.2f\n", (float)total_seek / n);
